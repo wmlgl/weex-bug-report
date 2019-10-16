4d52872a6167283e82e5f9a96e379a657039ea24
wmlgl <qq4014@126.com>
2019-10-17 02:30:36 +0800

    init

---
+++ src/components/sliderbug1.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <slider class="slider" :auto-pay="true">
+      <scroller class="scroller">
+        <text>AAA</text>
+        <image :src="imgA" style="width:300px;height:300px;background-color:orange" />
+      </scroller>
+      <scroller class="scroller">
+        <text>BBB</text>
+        <image :src="imgB" style="height:2000px" />
+      </scroller>
+      <scroller class="scroller">
+        <text>CCC</text>
+        <image :src="imgA" style="height:2000px" />
+      </scroller>
+    </slider>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      imgA: "local:///a",
+      imgB: "local:///b",
+      // imgC: "local:///c"
+      imgC:
+        "https://img.alicdn.com/tfs/TB1M2MYqzTpK1RjSZKPXXa3UpXa-1152-912.png"
+    };
+  }
+};
+</script>
+<style>
+.container {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+.slider {
+  flex: 1;
+}
+.scroller {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #eeeeee;
+  overflow: scroll;
+}
+</style>
---
+++ src/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <sliderbug1></sliderbug1>
+  </div>
+</template>
+
+<script>
+import sliderbug1 from "@/components/sliderbug1";
+export default {
+  name: "App",
+  components: {
+    sliderbug1
+  }
+};
+</script>
+
